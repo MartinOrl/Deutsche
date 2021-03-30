2319cc481d5a0af929892ab342382b3b3fc50f6e
MartinOrl <53393723+MartinOrl@users.noreply.github.com>
2021-03-30 20:16:34 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+
 import './App.css';
+import slova from './data'
 
 function App() {
+
+  const [mode, setMode] = useState(true)
+  const [searchVal, setSearch] = useState('')
+  const [display, setDisplay] = useState(slova)
+
+  // const filterData = (data, lan) => {
+  //   if(lan){
+  //     let res = slova.filter(slovo => {
+  //       console.log(slovo)
+  //       slovo[0].includes(search)
+  //     })
+  //     console.log(res)
+  //   }
+  // }
+
+  // let test = filterData(slova, mode)
+  let handleChange = (e) => {
+    let search = e.target.value
+    let res;
+    if(mode){
+      res = slova.filter(slovo => slovo[0].toLowerCase().includes(search.toLowerCase()))
+    }
+    else{
+      res = slova.filter(slovo => slovo[1].toLowerCase().includes(search.toLowerCase()))
+    }
+    
+    if(search === ''){
+      console.log("empty")
+      res = slova
+    }
+    setSearch(search)
+    setDisplay(res)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1>Deutsche Slovniken Pisomken</h1>
+      <div className='lanOpt'>
+        <p>Jazyk: {mode ? 'Nemčina' : 'Slovina'} </p>
+        <button className='lanChange' onClick={() => setMode(!mode)}>Zmeň na {mode ? 'slovinu' : 'nemčinu'}</button>
+      </div>
+      <div>
+        <input type="text" name="search" id="search" value={searchVal} onChange={handleChange}/>
+        <button className='inputRem' onClick={() => handleChange({target: {value: ''}})}>Vymazať</button>
+      </div>
+      
+      
+      <p className='searchTerm'>Hľadané: {searchVal}</p>
+
+      <div className="results">
+        {
+          display.map(slovo => {
+            return <p key={slovo[0]} >{slovo[0]} - {slovo[1]}</p>
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,139 @@
+const slova = [
+    [
+        "der Müllberg","skládka"
+    ],
+    [
+        "das Windrad", "veterná turbína"
+    ],
+    [
+        "die Luftverschmutzung","znečistenie vzduchu"
+    ],
+    [
+        "das Waldsterben", "odumieranie lesov"
+    ],
+    [
+        "die Mülltrenung","triedenie odpadu"
+    ],
+    [
+        "das Recycling", "recyklovanie"
+    ],
+    [
+        "der Sperrmüll","neskladný odpad"
+    ],
+    [
+        "der Abfall", "odpad"
+    ],
+    [
+        "das Kohlendioxid","oxid uhličitý"
+    ],
+    [
+        "erneuerbar", "obnoviteľný"
+    ],
+    [
+        "Energie nutzen","využiť energiu"
+    ],
+    [
+        "die Plastiktüte", "igelitové vrecko"
+    ],
+    [
+        "das Grundwasser","spodná voda"
+    ],
+    [
+        "vergiften", "otráviť"
+    ],
+    [
+        "die Mehrwegflasche", "zálohovaná fľaša"
+    ],
+    [
+        "umgeben","obklopovať"
+    ],
+    [
+        "das Fischsterben", "úhyn rýb"
+    ],
+    [
+        "sauer","kyslý"
+    ],
+    [
+        "die Tanker-Havarie", "havária tankera"
+    ],
+    [
+        "die Erwärmung", "otepľovanie"
+    ],
+    [
+        "sich leisten", "dovoliť si"
+    ],
+    [
+        "die Ressource","prírodný zdroj"
+    ],
+    [
+        "störend", "rušivý"
+    ],
+    [
+        "der Einfluss","vplyv"
+    ],
+    [
+        "die Eröffnung", "otvorenie, zahájenie"
+    ],
+    [
+        "der Recyclinghof", "zberný dvor"
+    ],
+    [
+        "der Mobilfunkanbeiter","mobilný operátor"
+    ],
+    [
+        "lediglich", "len, iba"
+    ],
+    [
+        "wiederverwenden","opätovne použiť"
+    ],
+    [
+        "der Benzinverbrauch", "spotreba benzínu"
+    ],
+    [
+        "die Entsorgung", "likvidácia odpadu"
+    ],
+    [
+        "umweltschädlich","škodlivý voči životnému prostrediu"
+    ],
+    [
+        "die Energiesparlampe", "úsporná žiarovka"
+    ],
+    [
+        "der Sondermüll","nebezpečný odpad"
+    ],
+    [
+        "opfern", "obetovať"
+    ],
+    [
+        "sich vorwerfen", "vyčítať si"
+    ],
+    [
+        "spülen", "splachovať"
+    ],
+    [
+        "stinken, stank","zapáchať"
+    ],
+    [
+        "giftig", "jedovatý"
+    ],
+    [
+        "umweltbewusst","ekologický"
+    ],
+    [
+        "der Strom", "prúd"
+    ],
+    [
+        "plastikfrei","bez plastov"
+    ],
+    [
+        "der Bedarf", "potreba"
+    ],
+    [
+        "anbringen","pripevniť"
+    ],
+    [
+        "der Müllwagen", "smetiarske auto"
+    ]
+]
+
+export default slova
